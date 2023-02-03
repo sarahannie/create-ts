edbd413b1c44bed482dcc70493fe4bc15cd4a605
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-02-03 12:06:15 +0100

    api of dog

--- src/feature/dogs/dog-api-slice.ts
+++ src/feature/dogs/dog-api-slice.ts
@@ -1,2 +1,16 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 const DOG_API_KEY = "live_Um54M5ThzcF5rlLDn660CEn1L0bUCzD5C7dc04kqB2IlONjIhuIEJTBDFHOxzAfe";
+
+interface Breed{
+    id:string;
+    name:string;
+    image:{
+        url:string;
+    }
+}
+
+
+
+const apislice = createApi({
+    
+})
